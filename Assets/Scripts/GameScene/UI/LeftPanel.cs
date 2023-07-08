ca2b0dff3a6264fa55937ef97bca29141533d59e
qox701 <765983658@qq.com>
2023-07-08 18:22:31 +0800

    完成了相机的旋转功能

---
+++ Assets/Scripts/GameScene/UI/LeftPanel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeftPanel : BasePanel
+{
+    public UnityEngine.UI.Button btnLeft;
+    public override void Init()
+    {
+        btnLeft.onClick.AddListener(() =>
+        {
+            
+        });
+    }
+}
---
+++ Assets/Scripts/GameScene/UI/RightPanel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightPanel : BasePanel
+{
+    public UnityEngine.UI.Button btnRight;
+    public override void Init()
+    {
+        btnRight.onClick.AddListener(() =>
+        {
+            
+        });
+    }
+}
